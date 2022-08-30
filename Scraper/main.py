80eaf6cbf14ca23f7756b7c0ac7fe472c44f6579
João Lucas Abreu <joaolucasabreunogueira@gmail.com>
2022-08-30 12:26:00 -0300

    up

---
+++ Scraper/main.py
@@ -0,0 +1,8 @@
+from ast import main
+import webscraper as wbs
+
+def main():
+    wbs('https://www.latamairlines.com/br/pt')
+
+if __name__ == '__main___':
+    main()
--- Scraper/webscraper.py
+++ Scraper/webscraper.py
@@ -23,6 +23,7 @@
 # No Windows, o PATH pode ser editado clicando com o botão direito sobre o ícone do
 # Computador (no Windows Explorer), depois no menu "Propriedades", em seguida "Configurações
 # avançadas do sistema" e, por fim, em "Variáveis de Ambiente".
+from os import link
 import sys
 import time
 from selenium import webdriver
@@ -30,6 +31,11 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+destinos = {}
+
+def __init__(self, link):
+	self.link = link
 
 def extrair_inteiro(texto):
 	try:
@@ -45,10 +51,30 @@ def extrair_inteiro(texto):
 
 		return int(sem_virgula)
 	except:
-		return 0
+		return 0 
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--incognito")
 driver = webdriver.Chrome()
-driver.get('https://google.com')
+driver.get('https://www.latamairlines.com/br/pt/oferta-voos?origin=GRU&inbound=null&outbound=2022-12-01T15%3A00%3A00.000Z&destination=SSA&adt=1&chd=0&inf=0&trip=OW&cabin=Economy&redemption=false&sort=RECOMMENDED')
+
+# Cookie
+cookiebtn = WebDriverWait(driver, 10).until(
+	EC.presence_of_element_located((By.ID, "cookies-politics-button"))
+)
+
+cookiebtn.click()
+
+# Tela 1
+# Origem
+origem = WebDriverWait(driver, 20).until(
+	EC.presence_of_element_located((By.ID, "undefined-dialog-open"))
+)
+
+origem.send_keys('São Paulo, GRU - Brasil')
+origem.send_keys(Keys.RETURN)
+
+
 
 input = WebDriverWait(driver, 20).until(
 	EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="q"]'))
@@ -62,18 +88,20 @@ link_imagens = WebDriverWait(driver, 20).until(
 )
 link_imagens.click()
 
-imagens = WebDriverWait(driver, 20).until(
+voos = WebDriverWait(driver, 20).until(
 	EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'img.rg_i'))
 )
 
 dados = []
 
-for imagem in imagens:
+""" for v in voos:
 	dados.append({
-		'descr': imagem.get_attribute('alt'),
-		'largura': int(imagem.get_attribute('width')),
-		'altura': int(imagem.get_attribute('height'))
-	})
+		'destino': imagem.get_attribute('alt'),
+		'hsaida': int(imagem.get_attribute('width')),
+		'hchegada': int(imagem.get_attribute('height')),
+		'duracao':,
+		'valor':
+	}) """
 
 print(dados)
 
