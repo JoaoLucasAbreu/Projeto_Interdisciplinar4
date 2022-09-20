40b8da8315800b43f6da27c79762b3b895a11848
BernardoCesar <bernardocesar2002@gmail.com>
2022-09-20 14:38:59 -0300

    novabase
    nova base ue

--- script.sql
+++ script.sql
@@ -1,38 +1,30 @@
 create database if not exists inter4;
 
-create table if not exists voo (
+use inter4;
+
+create table voo (
 idVoo int NOT NULL,
-duracao int NOT NULL,
-dataVoo datetime not null,
+origem varchar(3) NOT NULL,
+destino varchar(3) not null,
 primary key(idVoo)
 );
 
-create table if not exists passagem(
+create table passagem(
 idPassagem int not null,
 companhia varchar(20) not null,
 dataVisualizacao date not null,
-precoPesquisa number(5,2) not null,
-precoDesc number(5,2) null,
+dataVoo datetime not null,
+preco double(5,2) null,
+tpPreco double(5,2) not null,
 tpVoo varchar(15) not null,
+hrChegada datetime not null,
+hrSaida datetime not null,
+duracao time not null,
 idVoo_FK int not null,
 primary key(idPassagem),
 FOREIGN KEY (idVoo_FK) REFERENCES voo(idVoo)
 );
 
-create table if not exists destino(
-idDestino int not null,
-aeroporto varchar(3) not null,
-cidade varchar(50) not null,
-estado varchar(50) not null,
-chegada datetime not null,
-primary key(idDestino)
-);
 
-create table if not exists origem(
-idOrigem int not null,
-aeroporto varchar(3) not null,
-cidade varchar(50) not null,
-estado varchar(50) not null,
-saida datetime not null,
-primary key(idDestino)
-);
+
+
