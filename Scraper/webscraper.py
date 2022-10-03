926f2f2d26a486042145695b692ea4745e45c7da
Henrique dalla cort <henriquedallacort@hotmail.com>
2022-10-03 09:56:25 -0300

    Inserção no banco de dados das informações LATAM

--- Scraper/webscraper.py
+++ Scraper/webscraper.py
@@ -11,7 +11,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
-
 import sqlite3
 conn = sqlite3.connect('VOO_DB.db')
 cursor = conn.cursor()
@@ -30,8 +29,8 @@ idPassagem INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 idVoo INTEGER NOT NULL,
 companhia TEXT NOT NULL,
 media DOUBLE(5,2) NOT NULL,
-dataVoo DATE not null,
-dataPesquisa DATE not null,
+dataVoo TEXT not null,
+dataPesquisa TEXT not null,
 FOREIGN KEY(idVoo) REFERENCES voo(idVoo)
 )
 """)
@@ -56,7 +55,7 @@ chrome_options.add_argument("--incognito")
 driver = webdriver.Chrome(options=chrome_options)
 
 cookie = False
-lista = ['BSB','GIG','SSA','FLN','POA','REC','CWB','SDU','FOR','GYN','NVT','NAT','MCZ','FEN']
+lista = ['BSB','GIG','SSA','FLN','POA','REC','CWB','SDU','FOR','GYN','NVT','NAT','MCZ']
 for destinos in lista:
 	driver.get(f'https://www.latamairlines.com/br/pt/oferta-voos?origin=GRU&inbound=null&outbound=2022-12-01T15%3A00%3A00.000Z&destination={destinos}&adt=1&chd=0&inf=0&trip=OW&cabin=Economy&redemption=false&sort=RECOMMENDED')
 	driver.maximize_window()
@@ -82,27 +81,18 @@ for destinos in lista:
 		time.sleep(40)
 		print('tinha timer')
 
-	#Selecionando os destinos e a origem 
+	#Selecionando os destinos e a origem e inserindo na tabela de voo
+	conn = sqlite3.connect('VOO_DB.db')
+	cursor = conn.cursor()
 	origem= driver.find_element(By.XPATH ,'//*[@id="txtInputOrigin_field"]').get_attribute('value')
 	destino= driver.find_element(By.XPATH ,'//*[@id="txtInputDestination_field"]').get_attribute('value')
-	#cursor.execute('INSERT INTO voo(origem, destino) VALUES (?,?)', (origem, destino))
-	
+	cursor.execute('INSERT INTO voo(origem, destino) VALUES (?,?)', (origem, destino))
+	conn.commit()
+	conn.close()
 
-	#Inserindo dados gerais sobre a passagem 
-	companhia = 'LATAM'
-	dataVoo = driver.find_element(By.XPATH ,'//*[@id="departureDate"]').get_attribute('value')
-	dataPesquisa = datetime.datetime.now().date()
-
-	print(companhia)	
-	print(dataVoo)	
-	print(dataPesquisa)	
-	#idVoo =
-	# media = tem q pegar lá no laço
-	#cursor.execute('INSERT INTO passagem(companhia, dataVisualizacao,dataVoo,preco,tpPreco,tpVoo,hrChegada,hrSaida,duracao,idVoo_FK) VALUES (?,?)', (nome, idade))
 
 	valor_final = 0
 	i=0
-
 	try:
 		maior = driver.find_element(By.XPATH ,'//*[@id="WrapperCardFlight0"]/div/div[2]/div[2]/div/div/div/span/span[2]').get_attribute('innerHTML')
 		menor = driver.find_element(By.XPATH ,'//*[@id="WrapperCardFlight0"]/div/div[2]/div[2]/div/div/div/span/span[2]').get_attribute('innerHTML')
@@ -110,6 +100,8 @@ for destinos in lista:
 		maior = maior.replace(',','.')
 		menor = menor.replace('.','')
 		menor = menor.replace(',','.')
+		maiorI = 0
+		menorI = 0
 	except NoSuchElementException:
 			break
 
@@ -123,9 +115,11 @@ for destinos in lista:
 
 				if float(valor) > float(maior):
 					maior = float(valor)
+					maiorI = i
 
 				if float(valor) < float(menor):
 					menor = float(valor)	
+					menorI = i
 
 				valor_final += float(valor)   
 				duracao = driver.find_element(By.XPATH ,f'//*[@id="ContainerFlightInfo{i}"]/span[2]').get_attribute('innerHTML')     							
@@ -136,18 +130,43 @@ for destinos in lista:
 			break
 	if i >0:
 		valor_final /=i
-
-	print(f"Média de preço = {valor_final}")	
-	print(destino)
-	print(maior)	
-	print(menor)
-
-	#idVoo = cursor.execute('SELECT (idVoo) from voo WHERE destino = ?',[(destino)]).fetchone()
-	#cursor.execute('INSERT INTO passagem(idVoo,companhia,media,dataVoo,dataPesquisa) VALUES (?,?,?,?,?)', (idVoo[0],companhia,valor_final,dataVoo,dataPesquisa))
-
 	
-	#idPassagem = cursor.execute('SELECT (idPassagem) from passagem').fetchone()
-	#cursor.execute('INSERT INTO tp_passagem(idPassagem,hSaida ,hChegada,duracao ,preco) VALUES (?,?,?,?,?)', (idPassagem[0],hSaida ,hChegada,duracao ,preco))
+	#Inserindo dados gerais sobre a passagem 
+	conn = sqlite3.connect('VOO_DB.db')
+	cursor = conn.cursor()
+	companhia = 'LATAM'
+	dataVoo = driver.find_element(By.XPATH ,'//*[@id="departureDate"]').get_attribute('value')
+	
+	dataPesquisa = datetime.datetime.now().date()
+	print(dataPesquisa)
+	idVoo = cursor.execute('SELECT (idVoo) from voo WHERE destino = ?',[(destino)]).fetchone()
+	cursor.execute('INSERT INTO passagem(idVoo,companhia,media,dataVoo,dataPesquisa) VALUES (?,?,?,?,?)', (idVoo[0],companhia,valor_final,dataVoo,str(dataPesquisa)))
+	conn.commit()
+	conn.close()
+
+	conn = sqlite3.connect('VOO_DB.db')
+	cursor = conn.cursor()
+	idPassagem = cursor.execute('SELECT (idPassagem) from passagem WHERE idVoo =? AND dataPesquisa=?', (idVoo[0], str(dataPesquisa))).fetchone()
+	conn.commit()
+	conn.close()
+
+	i = maiorI
+	for j in range(2):
+		conn = sqlite3.connect('VOO_DB.db')
+		cursor = conn.cursor()
+		hChegada = driver.find_element(By.XPATH ,f'//*[@id="WrapperCardFlight{i}"]/div/div[1]/div[2]/div[1]/div[1]/span[1] ').text
+		hSaida =  driver.find_element(By.XPATH ,f'//*[@id="WrapperCardFlight{i}"]/div/div[1]/div[2]/div[1]/div[3]/span[1]').text
+		duracao =  driver.find_element(By.XPATH ,f'//*[@id="ContainerFlightInfo{i}"]/span[2]').text
+		preco =  driver.find_element(By.XPATH ,f'//*[@id="WrapperCardFlight{i}"]/div/div[1]/div[2]/div[2]/div/div/span/span[2]').text
+		print(hChegada)
+		print(hSaida)
+		print(duracao)
+		print(preco)
+		cursor.execute('INSERT INTO tp_passagem(idPassagem,hSaida ,hChegada,duracao ,preco) VALUES (?,?,?,?,?)', (idPassagem[0],hSaida ,hChegada,duracao ,preco))
+		i= menorI
+		conn.commit()
+		conn.close()
+
 
 driver.close()
 
--- script.sql
+++ script.sql
@@ -3,27 +3,31 @@ create database if not exists inter4;
 use inter4;
 
 create table if not exists voo (
-idVoo int NOT NULL,
-origem varchar(3) NOT NULL,
-destino varchar(3) not null,
-primary key(idVoo)
-);
+    idVoo INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    origem TEXT NOT NULL,
+    destino TEXT NOT NULL
+)
 
 create table if not exists passagem(
-idPassagem int not null,
-companhia varchar(20) not null,
-dataVisualizacao date not null,
-dataVoo datetime not null,
-preco double(5,2) null,
-tpPreco double(5,2) not null,
-tpVoo varchar(15) not null,
-hrChegada datetime not null,
-hrSaida datetime not null,
-duracao time not null,
-idVoo_FK int not null,
-primary key(idPassagem),
-FOREIGN KEY (idVoo_FK) REFERENCES voo(idVoo)
-);
+    idPassagem INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    idVoo INTEGER NOT NULL,
+    companhia TEXT NOT NULL,
+    media DOUBLE(5,2) NOT NULL,
+    dataVoo DATE not null,
+    dataPesquisa DATE not null,
+    FOREIGN KEY(idVoo) REFERENCES voo(idVoo)
+)
+
+create table if not exists tp_passagem(
+    idTpPassagem INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    idPassagem INTEGER NOT NULL,
+    hSaida TEXT NOT NULL,
+    hChegada TEXT NOT NULL,
+    duracao TEXT NOT NULL,
+    preco DOUBLE(5,2) NOT NULL,
+    FOREIGN KEY(idPassagem) REFERENCES passagem (idPassagem)
+)
+
 
 
 
