0747505745635bf81f0877b23409e35160cbdc09
João Lucas Abreu <joaolucasabreunogueira@gmail.com>
2022-11-13 01:51:19 -0300

    dashduplo + tables + npm

--- Dashboard/routes/api/voo.ts
+++ Dashboard/routes/api/voo.ts
@@ -1,23 +1,63 @@
 ﻿import { sql } from "teem";
 import app = require("teem");
 
+/* class VooApiRoute {
+	public static async listar(req: app.Request, res: app.Response) {
+
+		const iddestino = parseInt(req.query["d"] as string) || 0;
+		//const companhia = req.query["c"] || "";
+		
+		let labelsGol = null
+		let labelsLatam = null
+		let valoresGol = null
+		let valoresLatam = null
+
+		await sql.connect(async (sql) => {
+			labelsGol = await sql.query('select distinct DATE_FORMAT(dataPesquisa,"%e/%m") AS dataPesquisa from passagem  where companhia = 1 and idVoo = ? ' ,[iddestino]) as [] ;
+			labelsLatam = await sql.query('select distinct DATE_FORMAT(dataPesquisa,"%e/%m") AS dataPesquisa from passagem  where companhia = 2 and idVoo = ? ' ,[iddestino]) as [] ;
+			valoresGol = await sql.query("select media from passagem where companhia = 1 and idVoo = ? ", [iddestino]) as [] ; 
+			valoresLatam = await sql.query("select media from passagem where companhia = 2 and idVoo = ? ", [iddestino]) as [] ; 
+		});
+
+		let dados = {
+			labelsGol: labelsGol.map( x => x.dataPesquisa),
+			labelsLatam: labelsLatam.map( x => x.dataPesquisa),
+			valoresGol: valoresGol.map( x => x.media),
+			valoresLatam: valoresLatam.map( x => x.media),
+			
+		};
+
+		console.log(dados);
+		
+		res.json(dados);
+	}
+}
+
+export = VooApiRoute; */
+
 class VooApiRoute {
 	public static async listar(req: app.Request, res: app.Response) {
 
 		const iddestino = parseInt(req.query["d"] as string) || 0;
-		const companhia = req.query["c"] || "";
+		//const companhia = req.query["c"] || "";
 		
-		let labels = null
-		let valores = null
+		let labelsGol = null
+		let labelsLatam = null
+		let valoresGol = null
+		let valoresLatam = null
 
 		await sql.connect(async (sql) => {
-			labels = await sql.query('select distinct DATE_FORMAT(dataPesquisa,"%e/%m") AS dataPesquisa from passagem  where companhia = ? and idVoo = ? ' ,[companhia,iddestino]) as [] ;
-			valores = await sql.query("select media from passagem where companhia = ? and idVoo = ? ", [companhia,iddestino]) as [] ; 
+			labelsGol = await sql.query('select distinct DATE_FORMAT(dataPesquisa,"%e/%m") AS dataPesquisa from passagem  where companhia = ? and idVoo = ? ' ,["GOL",iddestino]) as [] ;
+			labelsLatam = await sql.query('select distinct DATE_FORMAT(dataPesquisa,"%e/%m") AS dataPesquisa from passagem  where companhia = ? and idVoo = ? ' ,["LATAM",iddestino]) as [] ;
+			valoresGol = await sql.query("select media from passagem where companhia = ? and idVoo = ? ", ["GOL",iddestino]) as [] ; 
+			valoresLatam = await sql.query("select media from passagem where companhia = ? and idVoo = ? ", ["LATAM",iddestino]) as [] ; 
 		});
 
 		let dados = {
-			labels: labels.map( x => x.dataPesquisa),
-			valores: valores.map( x => x.media),
+			labelsGol: labelsGol.map( x => x.dataPesquisa),
+			labelsLatam: labelsLatam.map( x => x.dataPesquisa),
+			valoresGol: valoresGol.map( x => x.media),
+			valoresLatam: valoresLatam.map( x => x.media),
 			
 		};
 		
